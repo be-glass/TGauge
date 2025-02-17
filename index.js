4c525562a7ab43fff2aeed538702dd18d0c47dd0
be-glass <be.glass@github.com>
2025-02-17 21:18:12 +0100

    export defaultConfig

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ import TGauge from "./src/components/TGauge.vue";
 export default TGauge; // ✅ Export the Vue component properly
 
 export { TGauge }; // ✅ Also allow named export
+
+
+import defaultConfig from "./src/default.js";
+export { defaultConfig };
